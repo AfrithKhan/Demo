5c76f35ef5562a8a4173b137e949bf8a9b530e9a
surya <surya.kandhasamy@softsuave.com>
2021-09-24 19:49:17 +0530

    m

--- index.js
+++ index.js
@@ -3,12 +3,9 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json())
 
-app.get('/', (req, res)=> {
-<<<<<<< HEAD
+app.get('/', (req, res)=> { HEAD
 res.send('Hello Hi _MY_ENV_VAR');
-=======
-res.send('Hello Manoj');
->>>>>>> 5a6ab725c699539ac559243ec26dfeb3140fdbea
+
 });
 
 app.post('/getIndexes', (req, res)=> {
