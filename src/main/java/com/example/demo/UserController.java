66662c8d4dc599a1176ae69d16621d06436157e6
manoj_softsuave <manoj.rajendran@softsuave.com>
2022-08-11 15:28:01 +0530

    deploy

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-    @GetMapping
+    @GetMapping("/")
     public String show()
     {
         return "Hello Java";
